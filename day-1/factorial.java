064ff05508f64b2008ae94c857cf93f8af1b308e
shashwat shukla <25114cn059@glbitm.ac.in>
2026-06-01 18:22:11 +0530

    Add files via upload

---
+++ day-1/factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class factorial 
+{
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number to find its factorial:");
+    int n= sc.nextInt();
+    double factorial = 1;
+
+    for (int i = 1; i <= n; i++) {
+        factorial =factorial *i;
+    }
+
+    System.out.println("Factorial of " + n + " is: " + factorial);
+}
+}
