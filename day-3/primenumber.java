397cc2b4f3a648a924184a47532444e23029d751
shashwat shukla <25114cn059@glbitm.ac.in>
2026-06-03 15:54:52 +0530

    Add files via upload

---
+++ day-3/primenumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class primenumber
+{
+    public static void main(String args[])
+    {
+        int n,i,count=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+         n=sc.nextInt();
+        i=1;
+        while (i<=n)
+        {
+            if(n%i==0)
+            {
+                count++;
+            }
+            i++;
+        }
+        if(count==2)
+        {
+            System.out.println("The number is prime");
+        }
+        else
+        {
+            System.out.println("The number is not prime");
+        }
+    }
+}   
