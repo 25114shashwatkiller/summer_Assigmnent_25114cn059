6eed41b8d069a4b61687caa06fb9558fabcf918c
shashwat shukla <25114cn059@glbitm.ac.in>
2026-06-08 16:55:40 +0530

    Add files via upload

---
+++ day-8/charactertriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class charactertriangle 
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                char ch = (char) ('A' + j - 1);
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
