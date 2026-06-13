372c81b1640623673188d29263bd1e869cd88a7a
shashwat shukla <25114cn059@glbitm.ac.in>
2026-06-13 17:58:01 +0530

    Add files via upload

---
+++ day-13/evenodd.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class evenodd 
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        
+        int[] array = new int[size];
+        
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
+        System.out.println("Even numbers in the array:");
+        for (int num : array) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+        
+        System.out.println("\nOdd numbers in the array:");
+        for (int num : array) {
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+        
+        scanner.close();
+    }
+}
